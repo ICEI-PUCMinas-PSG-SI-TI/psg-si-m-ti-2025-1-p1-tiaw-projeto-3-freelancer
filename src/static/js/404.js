b7e9bcaa6240a03404b49f6669a8bb37e3bc5053
mateusjdev <mateus.santana.1598679@sga.pucminas.br>
2025-05-07 13:20:13 -0300

    Layout dos scripts inserido dinamicamente

--- src/static/js/404.js
+++ src/static/js/404.js
@@ -2,21 +2,32 @@
 
 /*
  * Esse script captura todos os links na página que redirecionam para 404.html,
- * bloqueia o redirecionamento e mostra uma mensagem em toast informando que a
- * página ainda não foi implementada
+ * bloqueia o redirecionamento e adiciona um onClick que mostra uma mensagem em
+ * toast informando que a página ainda não foi implementada.
  * 
  */
 
 // https://getbootstrap.com/docs/5.3/components/toasts
 
-function setup404Toast(){
+function setup404Toast() {
+    const toast_container = document.createElement("div");
+    toast_container.classList.add("toast-container", "position-fixed", "bottom-0", "end-0", "p-3");
+    toast_container.innerHTML = `<div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-body">
+                Essa página ainda não foi implementada!
+            </div>
+        </div>`;
+
+    document.body.appendChild(toast_container);
+
     let toast = document.getElementById('liveToast')
+    // retorna todos os elementos <a> | href != null
     let document_links = document.links
     // @ts-ignore: bootstrap is imported in html
     let toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast)
 
     for (let i = 0; i < document_links.length; i++) {
-        if(document_links[i].href.endsWith("")){
+        if (document_links[i].href.endsWith("404.html")) {
             document_links[i].addEventListener('click', (event) => {
                 toastBootstrap.show()
                 event.preventDefault();
--- src/static/js/darkmode.js
+++ src/static/js/darkmode.js
@@ -11,6 +11,12 @@ function setupDarkMode(){
         return;
     }
 
+    toggleDarkDiv.classList.add("d-flex", "p-2", "flex-row", "justify-content-center", "align-items-center");
+    toggleDarkDiv.innerHTML += `<img class="dark-toggle" src="static/dark.svg">
+        <div class="form-check form-switch form-check-reverse">
+            <input id="toggleDark" class="form-check-input" type="checkbox">
+        </div>`;
+
     /** @type { HTMLInputElement | null } */
     // @ts-ignore: HTMLInputElement é derivado de HTMLElement
     let input = document.getElementById("toggleDark");
@@ -18,7 +24,6 @@ function setupDarkMode(){
         return;
     }
 
-    toggleDarkDiv.style.display = "block";
     const theme_key = "data-bs-theme";
 
     function isDarkTheme(){
@@ -52,6 +57,9 @@ function setupDarkMode(){
             setDarkTheme(true, true);
         }
     });
+
+    // if everything is ok, display toggle
+    toggleDarkDiv.classList.remove("d-none");
 }
 
 setupDarkMode();
