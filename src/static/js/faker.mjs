caaaccb83bfd3c819e3dcbfa1d14215a0a8fcccc
mateusjdev <git@mateusj.dev>
2025-06-09 10:09:01 -0300

    refactor: move getFakeData() to a class

--- src/static/js/faker.mjs
+++ src/static/js/faker.mjs
@@ -5,33 +5,48 @@ import * as JSONQL_U from "./jsonql.user.mjs"; // Usuários
 import * as JSONQL_C from "./jsonql.contract.mjs"; // Contratos
 import * as JSONQL_A from "./jsonql.review.mjs"; // Avaliações
 import * as JSONQL_P from "./jsonql.portfolio.mjs"; // Portfólios
-import { generateRandomNumberOld, ensureInteger } from "./tools.mjs";
+import { generateRandomNumberOld as generateRandomNumber, ensureInteger } from "./tools.mjs";
 
 /*
  * Esse script adiciona os recursos necessários para o funcionamento da página de dev-tools
  */
 
-let exemplos_json = null;
+class Exemplos {
+    avaliacoes = [];
+    biografia = [];
+    categorias_servicos = [];
+    cidades = [];
+    contatos = [];
+    cpf_cnpj = [];
+    descricoes = [];
+    email = [];
+    links_externos = [];
+    nomes = [];
+    sobrenomes = [];
+    tipo = [];
+}
 
-export async function getFakeData() {
-    if (exemplos_json) return exemplos_json;
+class ExemploFetcher {
+    json = null;
 
-    try {
-        // URL assume que você esta na página src/dev.html
-        const response = await fetch("exemplos");
+    /**
+     *
+     * @returns {Promise<Exemplos|null>}
+     */
+    async getFakeData() {
+        if (this.json) return this.json;
+
+        const response = await fetch("/exemplos");
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
 
-        const json = await response.json();
-        exemplos_json = json;
-        return json;
-    } catch (error) {
-        console.error(error.message);
+        this.json = await response.json();
+        return this.json;
     }
-
-    return null;
 }
+
+const exemplos = new ExemploFetcher();
 
 /**
  * Cria N portfólios
@@ -42,18 +57,34 @@ export async function criarNPortfolios(quantidade) {
     if (typeof quantidade_int !== "number" || quantidade_int <= 0)
         throw new Error("Criação de portfólios: A quantidade informada é inválida!");
 
-    await getFakeData().then((json) => {
-        const exemplos = json;
+    await exemplos.getFakeData().then((json) => {
+        if (!json) return;
 
         let portfolios = [];
 
         for (let index = 0; index < quantidade_int; index++) {
-            function createSecao(ordem) {
+            const usuarios = JSONQL_U.readUsuarios();
+
+            if (!usuarios || !usuarios.length)
+                throw new Error(
+                    "Criação de portfólios: É necessário que haja usuários cadastrados para criar portfólios.",
+                );
+
+            const userId_index = generateRandomNumber(usuarios.length);
+            if (!userId_index) continue;
+
+            // TODO: Avoid creating more than 1 portfolios per user
+            let secoes = [];
+
+            // Gera entre 2 e 5 seções para cada portfolio
+            const quant_secoes = generateRandomNumber(5, 2) || 2;
+
+            for (let ordem = 0; ordem < quant_secoes; ordem++) {
                 let ordem_int = ensureInteger(ordem);
                 let secao = {
                     ordem: ordem_int,
                     // TODO: Verificar dinamicamente as categorias possíveis
-                    categoriaId: generateRandomNumberOld(3),
+                    categoriaId: generateRandomNumber(3),
                     // TODO: Verificar dinamicamente os nomes de acordo com a categoria
                     nome: "Seção de Informações",
                 };
@@ -66,11 +97,11 @@ export async function criarNPortfolios(quantidade) {
                         {
                             // TODO: Adicionar imagens reais
                             secao.contents = [];
-                            const quant_imagens = generateRandomNumberOld(10, 5) || 5;
+                            const quant_imagens = generateRandomNumber(10, 5) || 5;
                             for (let j = 0; j < quant_imagens; j++) {
                                 secao.contents.push({
                                     blob: `https://picsum.photos/seed/${
-                                        generateRandomNumberOld(200) || "lucremais"
+                                        generateRandomNumber(200) || "lucremais"
                                     }/200`, // portfolios.secao.contents.blob - string
                                     descricao: "Foto", // portfolios.secao.contents.descricao - string
                                 });
@@ -90,15 +121,15 @@ export async function criarNPortfolios(quantidade) {
                     case 2:
                         {
                             secao.contents = [];
-                            const quant_links = generateRandomNumberOld(6, 3) || 3;
+                            const quant_links = generateRandomNumber(6, 3) || 3;
                             for (let j = 0; j < quant_links; j++) {
-                                const link_ext_index = generateRandomNumberOld(
-                                    exemplos.links_externos.length
+                                const link_ext_index = generateRandomNumber(
+                                    json.links_externos.length,
                                 );
 
                                 if (!link_ext_index) continue;
                                 secao.contents.push({
-                                    blob: exemplos.links_externos[link_ext_index], // portfolios.secao.contents.blob - string
+                                    blob: json.links_externos[link_ext_index], // portfolios.secao.contents.blob - string
                                     descricao: "Link Externo", // portfolios.secao.contents.descricao - string
                                 });
                                 secao.nome = "Redes Sociais";
@@ -108,28 +139,11 @@ export async function criarNPortfolios(quantidade) {
                         break;
                     default:
                         console.log("createNPortfolios: categoria informada não encontrada!");
-                        return null;
+                        break;
                 }
 
-                return secao;
+                secoes.push(secao);
             }
-
-            const usuarios = JSONQL_U.readUsuarios();
-
-            if (!usuarios || !usuarios.length)
-                throw new Error(
-                    "Criação de portfólios: É necessário que haja usuários cadastrados para criar portfólios."
-                );
-
-            const userId_index = generateRandomNumberOld(usuarios.length);
-            if (!userId_index) continue;
-
-            // TODO: Avoid creating more than 1 portfolios per user
-            let secoes = [];
-
-            // Gera entre 2 e 5 seções para cada portfolio
-            const quant_secoes = generateRandomNumberOld(5, 2) || 2;
-            for (let i = 0; i < quant_secoes; i++) secoes.push(createSecao(i));
 
             portfolios.push({
                 usuarioId: usuarios[userId_index].id, // number
@@ -158,22 +172,22 @@ export async function criarNContratos(quantidade) {
         const usuarios = JSONQL_U.readUsuarios();
         if (!usuarios || !usuarios.length)
             throw new Error(
-                "Criação de contratos: É necessário que haja usuários cadastrados para criar contratos."
+                "Criação de contratos: É necessário que haja usuários cadastrados para criar contratos.",
             );
 
         const servicos = JSONQL_S.readServicos();
         if (!servicos || !servicos.length)
             throw new Error(
-                "Criação de contratos: É necessário que haja serviços cadastrados para criar contratos."
+                "Criação de contratos: É necessário que haja serviços cadastrados para criar contratos.",
             );
 
-        const servicoId_index = generateRandomNumberOld(servicos.length);
-        const contratadoId_index = generateRandomNumberOld(usuarios.length);
-        const contratanteId_index = generateRandomNumberOld(usuarios.length);
+        const servicoId_index = generateRandomNumber(servicos.length);
+        const contratadoId_index = generateRandomNumber(usuarios.length);
+        const contratanteId_index = generateRandomNumber(usuarios.length);
 
-        const data_dia = generateRandomNumberOld(28, 1);
-        const data_mes = generateRandomNumberOld(12, 1);
-        const data_ano = generateRandomNumberOld(2026, 1970);
+        const data_dia = generateRandomNumber(28, 1);
+        const data_mes = generateRandomNumber(12, 1);
+        const data_ano = generateRandomNumber(2026, 1970);
 
         if (
             typeof servicoId_index !== "number" ||
@@ -192,7 +206,7 @@ export async function criarNContratos(quantidade) {
             contratadoId: usuarios[contratadoId_index].id, // number
             contratanteId: usuarios[contratanteId_index].id, // number
             data: `${data_dia}/${data_mes}/${data_ano}`, // string
-            valor: generateRandomNumberOld(5000, 1518) || 1518, // number
+            valor: generateRandomNumber(5000, 1518) || 1518, // number
         });
     }
 
@@ -210,8 +224,8 @@ export async function criarNAvaliacoes(quantidade) {
     if (typeof quantidade_int !== "number" || quantidade_int <= 0)
         throw new Error("Criação de avaliações: A quantidade informada é inválida!");
 
-    await getFakeData().then((json) => {
-        const exemplos = json;
+    await exemplos.getFakeData().then((json) => {
+        if (!json) return;
 
         let avaliacoes = [];
 
@@ -219,18 +233,18 @@ export async function criarNAvaliacoes(quantidade) {
             const usuarios = JSONQL_U.readUsuarios();
             if (!usuarios || !usuarios.length)
                 throw new Error(
-                    "Criação de avaliações: É necessário que haja usuários cadastrados para criar avaliações."
+                    "Criação de avaliações: É necessário que haja usuários cadastrados para criar avaliações.",
                 );
 
             const contratos = JSONQL_C.readContratos();
             if (!contratos || !contratos.length)
                 throw new Error(
-                    "Criação de avaliações: É necessário que haja contratos cadastrados para criar avaliações."
+                    "Criação de avaliações: É necessário que haja contratos cadastrados para criar avaliações.",
                 );
 
-            const contratoId_index = generateRandomNumberOld(contratos.length);
-            const contratanteId_index = generateRandomNumberOld(usuarios.length);
-            const comentario_index = generateRandomNumberOld(exemplos.avaliacoes.length);
+            const contratoId_index = generateRandomNumber(contratos.length);
+            const contratanteId_index = generateRandomNumber(usuarios.length);
+            const comentario_index = generateRandomNumber(json.avaliacoes.length);
 
             if (
                 typeof contratoId_index !== "number" ||
@@ -245,8 +259,8 @@ export async function criarNAvaliacoes(quantidade) {
             avaliacoes.push({
                 contratoId: contratos[contratoId_index].id, // number
                 contratanteId: usuarios[contratanteId_index].id, // number
-                nota: generateRandomNumberOld(11) || 0, // number
-                comentario: exemplos.avaliacoes[comentario_index], // string
+                nota: generateRandomNumber(11) || 0, // number
+                comentario: json.avaliacoes[comentario_index], // string
             });
         }
 
@@ -264,26 +278,26 @@ export async function criarNUsuarios(quantidade) {
     if (typeof quantidade_int !== "number" || quantidade_int <= 0)
         throw new Error("Criação de usuários: A quantidade informada é inválida!");
 
-    await getFakeData().then((json) => {
-        const exemplos = json;
+    await exemplos.getFakeData().then((json) => {
+        if (!json) return;
 
         let usuarios = [];
 
         for (let index = 0; index < quantidade_int; index++) {
-            const nome_index = generateRandomNumberOld(exemplos.nomes.length);
-            const sobrenomes_index = generateRandomNumberOld(exemplos.sobrenomes.length);
-            const foto_seed = generateRandomNumberOld(200);
-            const email_index = generateRandomNumberOld(exemplos.email.length);
-            const tipo_index = generateRandomNumberOld(exemplos.tipo.length);
-            const cpf_cnpj_index = generateRandomNumberOld(exemplos.cpf_cnpj.length);
-            const cidade_index = generateRandomNumberOld(exemplos.cidades.length);
-            const biografia_index = generateRandomNumberOld(exemplos.biografia.length);
-            const contato_1_index = generateRandomNumberOld(exemplos.contatos.length);
-            const contato_2_index = generateRandomNumberOld(exemplos.contatos.length);
+            const nome_index = generateRandomNumber(json.nomes.length);
+            const sobrenomes_index = generateRandomNumber(json.sobrenomes.length);
+            const foto_seed = generateRandomNumber(200);
+            const email_index = generateRandomNumber(json.email.length);
+            const tipo_index = generateRandomNumber(json.tipo.length);
+            const cpf_cnpj_index = generateRandomNumber(json.cpf_cnpj.length);
+            const cidade_index = generateRandomNumber(json.cidades.length);
+            const biografia_index = generateRandomNumber(json.biografia.length);
+            const contato_1_index = generateRandomNumber(json.contatos.length);
+            const contato_2_index = generateRandomNumber(json.contatos.length);
 
-            const data_nascimento_dia = generateRandomNumberOld(28, 1);
-            const data_nascimento_mes = generateRandomNumberOld(12, 1);
-            const data_nascimento_ano = generateRandomNumberOld(2006, 1970);
+            const data_nascimento_dia = generateRandomNumber(28, 1);
+            const data_nascimento_mes = generateRandomNumber(12, 1);
+            const data_nascimento_ano = generateRandomNumber(2006, 1970);
 
             if (
                 typeof nome_index !== "number" ||
@@ -306,16 +320,16 @@ export async function criarNUsuarios(quantidade) {
 
             usuarios.push({
                 ativo: true, // bool
-                nome: `${exemplos.nomes[nome_index]} ${exemplos.sobrenomes[sobrenomes_index]}`, // string
+                nome: `${json.nomes[nome_index]} ${json.sobrenomes[sobrenomes_index]}`, // string
                 foto: `https://picsum.photos/seed/${foto_seed}/200`, // string
                 data_nascimento: `${data_nascimento_dia}/${data_nascimento_mes}/${data_nascimento_ano}`, // string
-                email: exemplos.email[email_index], // string
-                senha: (generateRandomNumberOld(999999, 100000) || 123456).toString(), // string
-                tipo: exemplos.tipo[tipo_index], // string
-                cpf_cnpj: exemplos.cpf_cnpj[cpf_cnpj_index], // string
-                cidade: exemplos.cidades[cidade_index], // string
-                biografia: exemplos.biografia[biografia_index], // string
-                contatos: [exemplos.contatos[contato_1_index], exemplos.contatos[contato_2_index]], // Array
+                email: json.email[email_index], // string
+                senha: (generateRandomNumber(999999, 100000) || 123456).toString(), // string
+                tipo: json.tipo[tipo_index], // string
+                cpf_cnpj: json.cpf_cnpj[cpf_cnpj_index], // string
+                cidade: json.cidades[cidade_index], // string
+                biografia: json.biografia[biografia_index], // string
+                contatos: [json.contatos[contato_1_index], json.contatos[contato_2_index]], // Array
             });
         }
 
@@ -333,17 +347,15 @@ export async function criarNServicos(quantidade) {
     if (typeof quantidade_int !== "number" || quantidade_int <= 0)
         throw new Error("Criação de serviços: A quantidade informada é inválida!");
 
-    await getFakeData().then((json) => {
-        const exemplos = json;
+    await exemplos.getFakeData().then((json) => {
+        if (!json) return;
 
         let servicos = [];
 
         for (let index = 0; index < quantidade_int; index++) {
-            const categorias_servicos_index = generateRandomNumberOld(
-                exemplos.categorias_servicos.length
-            );
-            const contato_index = generateRandomNumberOld(exemplos.contatos.length);
-            const descricao_index = generateRandomNumberOld(exemplos.descricoes.length);
+            const categorias_servicos_index = generateRandomNumber(json.categorias_servicos.length);
+            const contato_index = generateRandomNumber(json.contatos.length);
+            const descricao_index = generateRandomNumber(json.descricoes.length);
 
             if (
                 typeof categorias_servicos_index !== "number" ||
@@ -355,12 +367,12 @@ export async function criarNServicos(quantidade) {
             }
 
             const element = {
-                titulo: exemplos.categorias_servicos[categorias_servicos_index],
+                titulo: json.categorias_servicos[categorias_servicos_index],
                 // TODO: categorias_servicos -> array, use id only
                 categoriaId: categorias_servicos_index,
-                categoria: exemplos.categorias_servicos[categorias_servicos_index],
-                contato: exemplos.contatos[contato_index],
-                descricao: exemplos.descricoes[descricao_index],
+                categoria: json.categorias_servicos[categorias_servicos_index],
+                contato: json.contatos[contato_index],
+                descricao: json.descricoes[descricao_index],
             };
 
             servicos.push(element);
