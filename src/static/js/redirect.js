21231e83757970bd5d7a0bd321a7441f24b1b261
mateusjdev <git@mateusj.dev>
2025-06-20 10:22:02 -0300

    feat: Adicionar funcionalidade de redirecionamento

---
+++ src/static/js/redirect.js
@@ -0,0 +1,32 @@
+const PATH_INDEX = "/";
+const PATH_HOMEPAGE = "/homepage";
+// @mock
+const PATH_AUTH = "/auth";
+
+// @mock
+const mock_isUserLoggedIn = () => false;
+
+// Redireciona para um caminho especifico dentro do mesmo host
+function redirectToPath(path) {
+    if (typeof path !== "string") throw new Error("redirectToPath: Invalid path");
+
+    const url = new URL(location.href);
+    const newUrl = `${url.origin}${path}`;
+
+    location.assign(newUrl);
+}
+
+function redirect() {
+    const url = new URL(window.location.href);
+    const urlPathname = url.pathname;
+
+    // INFO: 3001 = Sinaliza que esta rodando como desenvolvimento e desabilita todos os redirecionamentos
+    if (url.port === 3001) return;
+    if (mock_isUserLoggedIn()) {
+        if (urlPathname === PATH_INDEX || urlPathname === PATH_AUTH) redirectToPath(PATH_HOMEPAGE);
+    } else if (urlPathname !== PATH_INDEX && urlPathname !== PATH_AUTH) {
+        redirectToPath(PATH_INDEX);
+    }
+}
+
+redirect();
