0e3394874c9c8c96979213653e8b1e3893b3fc86
mateusjdev <git@mateusj.dev>
2025-06-20 22:06:51 -0300

    feat: Atualizar barras de navegação e adicionar modal do perfil de usuário

--- src/static/js/redirect.js
+++ src/static/js/redirect.js
@@ -1,6 +1,7 @@
 // TODO: Verificar necessidade de validar /*.html
 const PATH_INDEX = "/";
 const PATH_HOMEPAGE = "/homepage";
+const PATH_404 = "/404";
 // @mock
 const PATH_AUTH = "/auth";
 
@@ -22,7 +23,8 @@ function redirect() {
     const urlPathname = url.pathname;
 
     // INFO: 3001 = Sinaliza que esta rodando como desenvolvimento e desabilita todos os redirecionamentos
-    if (url.port === 3001) return;
+    if (url.port === "3001") return;
+    if (urlPathname === PATH_404) return;
     if (mock_isUserLoggedIn()) {
         if (urlPathname === PATH_INDEX || urlPathname === PATH_AUTH) redirectToPath(PATH_HOMEPAGE);
     } else if (urlPathname !== PATH_INDEX && urlPathname !== PATH_AUTH) {
