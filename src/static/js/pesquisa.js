2f50e6fc84f2b2e7449b8eb22477312af04c46fb
mateusjdev <git@mateusj.dev>
2025-06-22 02:46:28 -0300

    chore: Atualizar referências a página 404 e outros *.html

--- src/static/js/pesquisa.js
+++ src/static/js/pesquisa.js
@@ -15,9 +15,7 @@ function navigateToResultados(query) {
     if (query.length === 0) throw new Error("Pesquisa sem parâmetros!");
 
     const url = new URL(window.location.href);
-    let pathname = "";
-    if (url.pathname.includes("src")) pathname = "/src/resultados.html";
-    else pathname = "/resultados.html";
+    let pathname = "/resultados";
     const params = new URLSearchParams();
     params.set("q", query);
     const newUrl = `${url.origin}${pathname}?${params.toString()}`;
