804c3c3788a10b2c3ea687ec4dd6c7169bd48235
mateusjdev <git@mateusj.dev>
2025-06-23 17:25:36 -0300

    feat: Atualizar configurações do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,21 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
     { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
     { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+    {
+        rules: {
+            camelcase: "warn",
+            "consistent-return": "warn",
+            "dot-notation": "warn",
+            "no-inline-comments": "warn",
+            "no-lonely-if": "warn",
+            "no-nested-ternary": "warn",
+            "no-var": "warn",
+            "prefer-object-has-own": "warn",
+            radix: "warn",
+            "require-await": "warn",
+            "no-eval": "warn",
+            "no-continue": "warn",
+            eqeqeq: ["error", "always", { null: "ignore" }],
+        },
+    },
 ]);
